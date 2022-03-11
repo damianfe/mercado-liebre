e2a807c2d4ed9bb7383c4ef5837cba207b8a0fb7
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2022-03-11 10:05:49 -0300

    adding changes

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const port = 3030;
+/* Middlewares */
+app.use(express.static('public'))
+/* routes */
+app.get('/', (req, res)=> res.sendFile (path.resolve(__dirname, 'views', 'home.html')))
+/* server */
+app.listen(port, () => console.log(`Server running in http://localhost:${port}`))
+
