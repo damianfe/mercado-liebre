dba8f9e8d325a7c6c275189b51f99977af38dd37
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2022-03-15 11:05:10 -0300

    corrigiendo tarjetas

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const port = 3030;
+const port = 3040;
 /* Middlewares */
 app.use(express.static('public'))
 /* routes */
