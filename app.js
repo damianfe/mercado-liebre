bdb5866b0a140ad409d8640b8ed3ff297627146d
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2022-04-22 11:52:48 -0300

    agregando formularios

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 3040;
 app.use(express.static('public'))
 /* routes */
 app.get('/', (req, res)=> res.sendFile (path.resolve(__dirname, 'views', 'home.html')))
+app.get('/register', (req, res)=> res.sendFile (path.resolve(__dirname, 'views', 'register.html')))
+app.get('/login', (req, res)=> res.sendFile (path.resolve(__dirname, 'views', 'login.html')))
 /* server */
 app.listen(port, () => console.log(`Server running in http://localhost:${port}`))
 
